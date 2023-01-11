62485f29201ac76ecbd33b912487ff04a8b84324
adityayadav71 <adityayadav1088@gmail.com>
2023-01-11 20:59:04 +0530

    Added linter

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+// 1 - Middlewares
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2 - Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+process.env.NODE_ENV = process.argv[2].split('=')[1];
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
+
+mongoose.set('strictQuery', false);
 
 mongoose
   .connect(DB, {
@@ -21,27 +25,27 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    if (process.env.NODE_ENV === "development")
-      console.log("DB connection successful!");
+    if (process.env.NODE_ENV === 'DEV')
+      console.log('DB connection successful! 🖥️');
   });
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  if (process.env.NODE_ENV === "development")
-    console.log(`App running on port ${port}`);
+  if (process.env.NODE_ENV === 'DEV')
+    console.log(`App running on port ${port} ✅`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
 
-process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
-    console.log("💥 Process terminated!");
+    console.log('💥 Process terminated!');
   });
 });
