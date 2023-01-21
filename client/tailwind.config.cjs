019567bfa82fba38b8a6c2c5f877fe2ed49684bc
adityayadav71 <adityayadav1088@gmail.com>
2023-01-21 21:07:11 +0530

    configured tailwind colors and created home navbar

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['inter', 'serif'],
+      },
+      colors: {
+        primary: '#1E2023',
+        secondary: '#283647',
+        accent1: '#0098FA',
+        accent2: '#576577',
+        easyGreen: '#19EB48',
+        mediumYellow: '#E2BC1E',
+        hardRed: '#FF0000',
+        graphicLightBlue: '#0064FA',
+        graphicDarkBlue: '#0E5BCE',
+        grey1: '#A1ACBD',
+        grey2: '#D9D9D9',
+      }
+    },
   },
   plugins: [],
 };
