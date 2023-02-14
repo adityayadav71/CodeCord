56e454a747ffc779bf27a09d464f5b467355f13c
adityayadav71 <adityayadav1088@gmail.com>
2023-02-14 21:07:18 +0530

    added fixed navbar jsx

--- client/src/components/LandingPage/Hero.jsx
+++ client/src/components/LandingPage/Hero.jsx
@@ -1,9 +1,13 @@
 const Hero = (props) => {
   return (
     <div className="relative px-9 mt-11 mb-11 h-screen max-h-[720px]">
-      <h1 className="text-6xl w-4/6 leading-[5rem] font-bold">Communicate, Practice, and Improve Your Coding Skills</h1>
+      <h1 className="text-6xl w-4/6 leading-[5rem] font-bold">
+        Communicate, Practice, and Improve Your Coding Skills
+      </h1>
       <p className="text-2xl text-grey2 w-1/2 leading-[2rem]  mt-5">
-        Our platform enables software developers to practice, collaborate, and improve their coding skills through a range of tools, resources, and incentives.
+        Our platform enables software developers to practice, collaborate, and
+        improve their coding skills through a range of tools, resources, and
+        incentives.
       </p>
       <div className="absolute z-[-1] bg-graphicLightBlue w-[1801.5px] h-[1273.94px] -top-[64.677%] left-[69.677%] origin-center -rotate-[42deg]"></div>
       <div className="absolute z-[-1] bg-graphicDarkBlue w-[472.59px] h-[545.4px] top-[23%] left-[90%] origin-center -rotate-[42deg]"></div>
--- client/src/components/LandingPage/index.jsx
+++ client/src/components/LandingPage/index.jsx
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import Feature from "./Feature";
 import Footer from "./Footer";
+// import { Link } from "react-router-dom";
 
 const LandingPage = (props) => {
   return (
@@ -50,6 +51,29 @@ const LandingPage = (props) => {
         glow="feature1"
       />
       <Footer />
+      {/* {!isAtTop && (
+        <nav className="transition duration-300 fixed top-0 left-0 flex flex-row gap-x-3 items-center justify-between bg-primary w-full">
+          <ul className="flex flex-row items-center">
+            <li className="p-6">Collaborate</li>
+            <li className="p-6">Compete</li>
+            <li className="p-6">Solve</li>
+          </ul>
+          <div className="flex flex-row items-center gap-x-6 ml-auto">
+            <Link
+              to="/app/auth/login"
+              className="p-3 w-36 text-xl transition-all ease-in-out duration-300 hover:cursor-pointer hover:scale-110 border border-white text-white text-center font-bold rounded-xl hover:shadow-signUp hover:shadow"
+            >
+              Login
+            </Link>
+            <Link
+              to="/app/auth/signup"
+              className="p-3 w-36 text-xl transition-all ease-in-out duration-300 hover:cursor-pointer hover:scale-110 bg-accent1 text-white text-center font-bold rounded-xl hover:shadow-signUp hover:shadow"
+            >
+              Sign up
+            </Link>
+          </div>
+        </nav>
+      )} */}
     </>
   );
 };
