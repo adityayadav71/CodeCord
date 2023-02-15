06eb480ba4a49ddbbb5ac72cc9b2e271f4be18ad
adityayadav71 <adityayadav1088@gmail.com>
2023-02-15 20:59:06 +0530

    completed adding sticky navbar on landing page

--- client/src/components/LandingPage/index.jsx
+++ client/src/components/LandingPage/index.jsx
@@ -1,48 +1,81 @@
 import Hero from "./Hero";
 import Feature from "./Feature";
 import Footer from "./Footer";
-// import { Link } from "react-router-dom";
+import { InView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const LandingPage = (props) => {
+  const [isAtTop, setIsAtTop] = useState(true);
+  const [activeSection, setActiveSection] = useState("");
+
   return (
     <>
-      <Hero />
-      <Feature
-        supTitle="Collaborate"
-        TitleHighLight="Create rooms"
-        Title=" and invite your friends to compete"
-        color="accent1"
-        gradient="featureGradient1"
-        glow="feature1"
-        list={[
-          "Set a timer and solve a set amount of problems.",
-          "Use score board to compare performance.",
-          "Go through everyone’s code after the contest ends.",
-          "Discuss with others in the room on call.",
-        ]}
-      />
-      <Feature
-        supTitle="Compete"
-        TitleHighLight="Attend Weekly Global Contests"
-        Title=" and improve your contest rating"
-        color="easyGreen"
-        gradient="featureGradient2"
-        glow="feature2"
-        extra="noText"
-      />
-      <Feature
-        supTitle="Solve"
-        TitleHighLight="Filter problems by topics, difficulty "
-        Title=" and solve them"
-        color="mediumYellow"
-        gradient="featureGradient3"
-        glow="feature3"
-        list={[
-          "Solve problems independently.",
-          "Solve problems asked in big tech company interviews.",
-          "Use tags to include specific type of problems in contest rooms.",
-        ]}
-      />
+      <InView
+        threshold={[0]}
+        onChange={(inView) => {
+          setIsAtTop(inView);
+        }}
+      >
+        <Hero />
+      </InView>
+      <InView
+        threshold={[0.5]}
+        onChange={(inView) => {
+          if (inView) setActiveSection("Collaborate");
+        }}
+      >
+        <Feature
+          supTitle="Collaborate"
+          TitleHighLight="Create rooms"
+          Title=" and invite your friends to compete"
+          color="accent1"
+          gradient="featureGradient1"
+          glow="feature1"
+          list={[
+            "Set a timer and solve a set amount of problems.",
+            "Use score board to compare performance.",
+            "Go through everyone’s code after the contest ends.",
+            "Discuss with others in the room on call.",
+          ]}
+        />
+      </InView>
+      <InView
+        threshold={[0.5]}
+        onChange={(inView) => {
+          if (inView) setActiveSection("Compete");
+        }}
+      >
+        <Feature
+          supTitle="Compete"
+          TitleHighLight="Attend Weekly Global Contests"
+          Title=" and improve your contest rating"
+          color="easyGreen"
+          gradient="featureGradient2"
+          glow="feature2"
+          extra="noText"
+        />
+      </InView>
+      <InView
+        threshold={[0.5]}
+        onChange={(inView) => {
+          if (inView) setActiveSection("Solve");
+        }}
+      >
+        <Feature
+          supTitle="Solve"
+          TitleHighLight="Filter problems by topics, difficulty "
+          Title=" and solve them"
+          color="mediumYellow"
+          gradient="featureGradient3"
+          glow="feature3"
+          list={[
+            "Solve problems independently.",
+            "Solve problems asked in big tech company interviews.",
+            "Use tags to include specific type of problems in contest rooms.",
+          ]}
+        />
+      </InView>
       <Feature
         supTitle="Sign up for an account"
         color="accent1"
@@ -51,12 +84,27 @@ const LandingPage = (props) => {
         glow="feature1"
       />
       <Footer />
-      {/* {!isAtTop && (
-        <nav className="transition duration-300 fixed top-0 left-0 flex flex-row gap-x-3 items-center justify-between bg-primary w-full">
-          <ul className="flex flex-row items-center">
-            <li className="p-6">Collaborate</li>
-            <li className="p-6">Compete</li>
-            <li className="p-6">Solve</li>
+      {!isAtTop && (
+        <nav className="drop-shadow-lg transition duration-300 fixed top-0 left-0 flex flex-row gap-x-3 items-center justify-between bg-primary w-full">
+          <ul className="flex flex-row items-center text-md ml-6">
+            <li
+              className={`m-4 p-2
+              ${activeSection === "Collaborate" ? "border-b border-b-white" : ""}`}
+            >
+              Collaborate
+            </li>
+            <li
+              className={`m-4 p-2
+              ${activeSection === "Compete" ? "border-b border-b-white" : ""}`}
+            >
+              Compete
+            </li>
+            <li
+              className={`m-4 p-2
+              ${activeSection === "Solve" ? "border-b border-b-white" : ""}`}
+            >
+              Solve
+            </li>
           </ul>
           <div className="flex flex-row items-center gap-x-6 ml-auto">
             <Link
@@ -73,7 +121,7 @@ const LandingPage = (props) => {
             </Link>
           </div>
         </nav>
-      )} */}
+      )}
     </>
   );
 };
