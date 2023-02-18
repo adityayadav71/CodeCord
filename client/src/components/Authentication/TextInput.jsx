fc98f5f5f28b56725bd72db2e1407f4e6a04ec5f
adityayadav71 <adityayadav1088@gmail.com>
2023-02-18 18:28:32 +0530

    removed common Form component

--- client/src/components/Authentication/TextInput.jsx
+++
@@ -1,7 +0,0 @@
-const TextInput = ({placeholder}) => {
-  return (
-    <input className="w-full rounded-xl h-18 px-6 py-6 text-base focus:outline focus:outline-accent1 bg-grey3" placeholder={`${placeholder}`}></input>
-  )
-};
-
-export default TextInput;
