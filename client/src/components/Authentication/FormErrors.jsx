2f4538c2fea6d8f243f535e79e85483fbe615ab1
adityayadav71 <adityayadav1088@gmail.com>
2023-02-20 13:02:35 +0530

    modified styles for FormErrors component

--- client/src/components/Authentication/FormErrors.jsx
+++ client/src/components/Authentication/FormErrors.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import { MdCancel } from "react-icons/md";
 
-const FormErrors = ({ name, message }) => {
+const FormErrors = ({ message }) => {
   return (
-    <div className="flex flex-col w-full gap-y-2 bg-lightPrimary border border-hardRed p-3 rounded-lg">
-      {name && <h1 className="text-hardRed text-xl font-bold">{name}</h1>}
-      <p className="text-md text-red-600 font-bold">{message}</p>
+    <div className="flex flex-row items-center gap-x-3 w-full gap-y-2 bg-lightPrimary border border-hardRed p-3 rounded-lg">
+      <MdCancel className="text-3xl text-hardRed" />
+      <p className="text-md leading-6 text-red-600 font-bold">{message}</p>
     </div>
   );
 };
