1e44c7aae145aa6585b37f0c195f97e144625580
adityayadav71 <adityayadav1088@gmail.com>
2023-02-26 10:15:55 +0530

    fixed mongoose validation error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -192,7 +192,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordConfirm = req.body.passwordConfirm;
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
-  user.save();
+  await user.save();
 
   // 3) Update the passwordChangedAt property
   // 4) Log the user in by sending the JWT
