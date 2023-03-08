6b8ab81f36bcf33369817d5ade9b9d6c8dfd798b
adityayadav71 <adityayadav1088@gmail.com>
2023-03-08 21:03:24 +0530

    added not found route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -72,6 +72,7 @@ function App() {
                 <Route path="reset/:token" element={<PasswordReset />} />
               </Route>
             </Route>
+            <Route path="/notfound" element={<NotFound />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </>
