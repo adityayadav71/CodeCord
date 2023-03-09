41537d6f56af2866611154f87592f4b023ae3cb3
adityayadav71 <adityayadav1088@gmail.com>
2023-03-09 18:57:17 +0530

    updated to send username as query param

--- client/src/api/profileDataAPI.js
+++ client/src/api/profileDataAPI.js
@@ -2,27 +2,12 @@ import axios from "axios";
 import { BASE_URL } from "./apiConfig";
 
 export const getUserData = async (username) => {
-  const response = await axios.get(
-    `${BASE_URL}/api/v1/users/profile?username=${username}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await axios.get(`${BASE_URL}/api/v1/users/profile?username=${username}`);
   return response.data;
 };
 
 export const createUserProfile = async (username) => {
-  await axios.post(
-    `${BASE_URL}/api/v1/users/profile`,
-    JSON.stringify({ username: username }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  await axios.post(`${BASE_URL}/api/v1/users/profile?username=${username}`);
 };
 
 export const updateUserProfile = async (data) => {
