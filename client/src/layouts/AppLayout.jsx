7cef61e4dcddc277f0052f50ff8c87a38bd3bf2c
adityayadav71 <adityayadav1088@gmail.com>
2023-03-15 07:16:20 +0530

    created RoomContext

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,16 +1,27 @@
+import { createContext, useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import Navbar from "../components/HomePage/Navbar";
 import Copyright from "../utilities/Copyright";
+import { io } from "socket.io-client";
+
+export const RoomContext = createContext(null);
 
 const AppLayout = ({ handleLogout }) => {
   const params = useParams();
+  const [connection, setConnection] = useState(null);
 
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    setConnection(socket);
+  }, []);
   return (
-    <div className={`flex flex-col ${params?.name ? "h-screen" : "h-full"}`}>
-      <Navbar handleLogout={handleLogout}/>
-      <Outlet />
-      {!params?.name && <Copyright />}
-    </div>
+    <RoomContext.Provider value={{ connection, setConnection }}>
+      <div className={`flex flex-col ${params?.name ? "h-screen" : "h-full"}`}>
+        <Navbar handleLogout={handleLogout} />
+        <Outlet />
+        {!params?.name && <Copyright />}
+      </div>
+    </RoomContext.Provider>
   );
 };
 
