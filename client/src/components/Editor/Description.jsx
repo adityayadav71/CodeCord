c1c8543e679f41389e1b9cfe75399977ea7d7866
adityayadav71 <adityayadav1088@gmail.com>
2023-03-22 14:26:18 +0530

    Created ProblemContext and populated problem data

--- client/src/components/Editor/Description.jsx
+++ client/src/components/Editor/Description.jsx
@@ -1,28 +1,49 @@
-import { useState } from "react";
-import { AiFillLike, AiFillDislike, AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import { useContext, useState } from "react";
+import {
+  AiFillLike,
+  AiFillDislike,
+  AiOutlineLike,
+  AiOutlineDislike,
+} from "react-icons/ai";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+import { ProblemContext } from "./index";
 
 const Description = ({ isRoom }) => {
+  const { problems } = useContext(ProblemContext);
   const [activeTab, setActiveTab] = useState("Description");
   return (
     <div className="flex flex-col h-full">
       {!isRoom && (
         <nav className="flex items-center justify-between px-3 py-3 border-b border-lightAccent3">
-          <button className={`grow px-6 py-2 ${activeTab === "Description" ? "bg-lightSecondary text-white" : ""} text-grey2 hover:text-white rounded-lg`} onClick={() => setActiveTab("Description")}>
+          <button
+            className={`grow px-6 py-2 ${
+              activeTab === "Description" ? "bg-lightSecondary text-white" : ""
+            } text-grey2 hover:text-white rounded-lg`}
+            onClick={() => setActiveTab("Description")}
+          >
             Description
           </button>
-          <button className={`grow px-6 py-2 ${activeTab === "Submissions" ? "bg-lightSecondary text-white" : ""} text-grey2 hover:text-white rounded-lg`} onClick={() => setActiveTab("Submissions")}>
+          <button
+            className={`grow px-6 py-2 ${
+              activeTab === "Submissions" ? "bg-lightSecondary text-white" : ""
+            } text-grey2 hover:text-white rounded-lg`}
+            onClick={() => setActiveTab("Submissions")}
+          >
             Submissions
           </button>
-          <button className={`grow px-6 py-2 ${activeTab === "Solutions" ? "bg-lightSecondary text-white" : ""} text-grey2 hover:text-white rounded-lg`} onClick={() => setActiveTab("Solutions")}>
+          <button
+            className={`grow px-6 py-2 ${
+              activeTab === "Solutions" ? "bg-lightSecondary text-white" : ""
+            } text-grey2 hover:text-white rounded-lg`}
+            onClick={() => setActiveTab("Solutions")}
+          >
             Solutions
           </button>
         </nav>
       )}
       <div className="overflow-y-scroll px-3 pt-6">
         <div className="flex flex-row justify-between items-center">
-          <h1 className="text-2xl mb-2">1. Problem Title</h1>
+          <h1 className="text-2xl mb-2">{problems?.number}. {problems?.title}</h1>
           {isRoom && (
             <div className="flex item-center gap-x-3">
               <button className="p-2 rounded-lg bg-grey3 hover:bg-accent1 transition-all duration-300">
@@ -35,7 +56,9 @@ const Description = ({ isRoom }) => {
           )}
         </div>
         <div className="flex flex-row items-center gap-x-5 mb-6">
-          <div className="px-3 bg-tagYellowBg text-mediumYellow font-bold rounded-full">Medium</div>
+          <div className="px-3 bg-tagYellowBg text-mediumYellow font-bold rounded-full">
+            {problems?.difficulty}
+          </div>
           <div className="flex flex-row gap-x-1 items-center">
             <AiOutlineLike className="text-xl" />
             <p className="text-grey1 font-bold">25.6K</p>
@@ -45,30 +68,29 @@ const Description = ({ isRoom }) => {
             <p className="text-grey1 font-bold">2K</p>
           </div>
         </div>
-        <div className="mb-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus non ex molestiae ipsam laboriosam placeat exercitationem corporis omnis id incidunt hic modi, soluta animi nemo.
-          Numquam eius fugiat cum.
-        </div>
-        <div className="mb-6">
-          <h1 className="mb-6 font-bold">Example 1:</h1>
-          <div className="px-3 py-2 rounded-lg bg-lightAccent3">
-            <span className="font-bold">Input:</span> s = "abcabcbb" <br /> <span className="font-bold">Output:</span> 3 <br />
-            <span className="font-bold">Explanation:</span> The answer is "abc", with the length of 3.
+        <div className="mb-6">{problems?.statement}</div>
+        {problems?.example?.map((example, i) => (
+          <div className="mb-6" key={i}>
+            <h1 className="mb-6 font-bold">Example {i + 1}:</h1>
+            <div className="px-3 py-2 rounded-lg bg-lightAccent3">
+              <span className="font-bold">Input:</span> {example.input} <br />
+              <span className="font-bold">Output:</span> {example.output} <br />
+              {example?.explanation && (
+                <>
+                  <span className="font-bold">Explanation:</span>
+                  {example?.explanation}
+                </>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="mb-6">
-          <h1 className="mb-6 font-bold">Example 2:</h1>
-          <div className="px-3 py-2 rounded-lg bg-lightAccent3">
-            <span className="font-bold">Input:</span> s = "abcabcbb" <br /> <span className="font-bold">Output:</span> 3 <br />
-            <span className="font-bold">Explanation:</span> The answer is "abc", with the length of 3.
-          </div>
-        </div>
+        ))}
         <div className="mb-6">
           <h1 className="mb-3 font-bold">Constraints:</h1>
           <div className="px-3 py-2">
             <ul className="list-disc list-inside">
-              <li>{`0 <= s.length <= 5 * 104`}</li>
-              <li>s consists of English letters, digits, symbols and spaces.</li>
+              {problems?.constraints?.map((constraint, i) => (
+                <li key={i}>{constraint}</li>
+              ))}
             </ul>
           </div>
         </div>
--- client/src/components/Editor/index.jsx
+++ client/src/components/Editor/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import Split from "react-split";
 import { FaAngleUp, FaRegTimesCircle } from "react-icons/fa";
 import Description from "./Description";
@@ -8,12 +8,15 @@ import Chat from "../Rooms/Chat";
 import LanguageSelector from "./LanguageSelector";
 import { RoomContext } from "../../layouts/AppLayout";
 import { AuthContext } from "../../App";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import * as themes from "@uiw/codemirror-themes-all";
 import ThemeSelector from "./ThemeSelector";
 import FontSelector from "./FontSelector";
 import KeyBindSelector from "./KeyBindSelector";
 import TabSelector from "./TabSelector";
+import { getProblem } from "../../api/problemDataAPI";
+
+export const ProblemContext = createContext(null);
 
 const Editor = ({ isRoom }) => {
   const editorRef = useRef(null);
@@ -31,10 +34,22 @@ const Editor = ({ isRoom }) => {
     tabSize: 2,
     value: localStorage.getItem("editorValue") || "",
   });
+  const [problems, setProblems] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    const loadProblems = async () => {
+      const response = await getProblem(params.name);
+      console.log(response);
+      setProblems(response.problem);
+    };
+    loadProblems();
+  }, []);
 
   useEffect(() => {
     const sizes = JSON.parse(localStorage.getItem("sizes"));
-    if ((isRoom && sizes?.length === 3) || (!isRoom && sizes?.length === 2)) setSizes(sizes);
+    if ((isRoom && sizes?.length === 3) || (!isRoom && sizes?.length === 2))
+      setSizes(sizes);
   }, []);
 
   useEffect(() => {
@@ -65,41 +80,74 @@ const Editor = ({ isRoom }) => {
   };
 
   return (
-    <>
-      <Split className="editor flex flex-row grow overflow-hidden h-full" onDrag={updateSize} sizes={sizes} minSize={[0, 500, 0]} maxSize={[2560, 2560, 250]} snapOffset={[300, 0, 200]}>
+    <ProblemContext.Provider value={{ problems }}>
+      <Split
+        className="editor flex flex-row grow overflow-hidden h-full"
+        onDrag={updateSize}
+        sizes={sizes}
+        minSize={[0, 500, 0]}
+        maxSize={[2560, 2560, 250]}
+        snapOffset={[300, 0, 200]}
+      >
         <div className="bg-transparentSecondary overflow-x-hidden">
           <Description isRoom={isRoom} />
         </div>
         <div>
-          <Split style={{ height: "calc(100% - 56px)" }} direction="vertical" sizes={consoleOpen ? [70, 30] : [100, 0]} minSize={[260, 0]} snapOffset={[0, 100]}>
+          <Split
+            style={{ height: "calc(100% - 56px)" }}
+            direction="vertical"
+            sizes={consoleOpen ? [70, 30] : [100, 0]}
+            minSize={[260, 0]}
+            snapOffset={[0, 100]}
+          >
             <div ref={editorRef} className="z-[-1] h-full bg-primary">
               <CodeEditor isRoom={isRoom} editorSettings={editorSettings} />
             </div>
             <div className="bg-lightAccent3 z-10">
-              <Console handleSettings={handleSettings} clearEditor={handleClearEditor} />
+              <Console
+                handleSettings={handleSettings}
+                clearEditor={handleClearEditor}
+              />
             </div>
           </Split>
           <div className="flex flex-row items-center bg-lightAccent3 justify-between p-3 h-[56px] font-bold">
             <div className="flex flex-row items-center gap-x-3">
-              <button className="flex flex-row items-center justify-between gap-x-3 px-3 py-1 bg-primary hover:bg-lightPrimary rounded-lg" onClick={() => setConsoleOpen((prev) => !prev)}>
+              <button
+                className="flex flex-row items-center justify-between gap-x-3 px-3 py-1 bg-primary hover:bg-lightPrimary rounded-lg"
+                onClick={() => setConsoleOpen((prev) => !prev)}
+              >
                 <p>Console</p>
                 <FaAngleUp className={`${consoleOpen ? "rotate-180" : ""}`} />
               </button>
-              <LanguageSelector editorSettings={editorSettings} setEditorSettings={setEditorSettings} />
+              <LanguageSelector
+                editorSettings={editorSettings}
+                setEditorSettings={setEditorSettings}
+              />
             </div>
             <div className="flex flex-row items-center gap-x-3">
               {!isLoggedIn ? (
                 <p>
                   Please{" "}
-                  <Link to="/app/auth/login" className="text-blue-500 font-bold hover:underline">
+                  <Link
+                    to="/app/auth/login"
+                    className="text-blue-500 font-bold hover:underline"
+                  >
                     Log in/Signup
                   </Link>{" "}
                   to run or submit your code
                 </p>
               ) : (
                 <>
-                  <button className={`px-4 py-1 bg-primary hover:bg-lightPrimary rounded-lg`}>Run</button>
-                  <button className={`px-4 py-1 bg-green hover:bg-easyGreen rounded-lg`}>Submit</button>
+                  <button
+                    className={`px-4 py-1 bg-primary hover:bg-lightPrimary rounded-lg`}
+                  >
+                    Run
+                  </button>
+                  <button
+                    className={`px-4 py-1 bg-green hover:bg-easyGreen rounded-lg`}
+                  >
+                    Submit
+                  </button>
                 </>
               )}
             </div>
@@ -115,27 +163,42 @@ const Editor = ({ isRoom }) => {
         <div className="settings absolute z-[9999] p-6 shadow shadow-modal top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 bg-secondary rounded-lg">
           <div className="flex items-center justify-between mb-6">
             <h1 className="text-2xl font-bold">Settings</h1>
-            <FaRegTimesCircle className="text-2xl font-bold hover:cursor-pointer" onClick={() => setSettingsOpen(!setSettingsOpen)} />
+            <FaRegTimesCircle
+              className="text-2xl font-bold hover:cursor-pointer"
+              onClick={() => setSettingsOpen(!setSettingsOpen)}
+            />
           </div>
           <div className="flex items-center justify-between p-3">
             <h1 className="text-lg">Editor Theme</h1>
-            <ThemeSelector editorSettings={editorSettings} setEditorSettings={setEditorSettings} />
+            <ThemeSelector
+              editorSettings={editorSettings}
+              setEditorSettings={setEditorSettings}
+            />
           </div>
           <div className="flex items-center justify-between p-3">
             <h1 className="text-lg">Font Size</h1>
-            <FontSelector editorSettings={editorSettings} setEditorSettings={setEditorSettings} />
+            <FontSelector
+              editorSettings={editorSettings}
+              setEditorSettings={setEditorSettings}
+            />
           </div>
           <div className="flex items-center justify-between p-3">
             <h1 className="text-lg">Key Bindings</h1>
-            <KeyBindSelector editorSettings={editorSettings} setEditorSettings={setEditorSettings} />
+            <KeyBindSelector
+              editorSettings={editorSettings}
+              setEditorSettings={setEditorSettings}
+            />
           </div>
           <div className="flex items-center justify-between p-3">
             <h1 className="text-lg">Tab Size</h1>
-            <TabSelector editorSettings={editorSettings} setEditorSettings={setEditorSettings} />
+            <TabSelector
+              editorSettings={editorSettings}
+              setEditorSettings={setEditorSettings}
+            />
           </div>
         </div>
       )}
-    </>
+    </ProblemContext.Provider>
   );
 };
 
