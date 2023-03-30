72196a11f24683c117139de467177468dc9643f5
adityayadav71 <adityayadav1088@gmail.com>
2023-03-30 13:46:48 +0530

    Added filterContext to problems page

--- client/src/components/Problems/ProblemList.jsx
+++ client/src/components/Problems/ProblemList.jsx
@@ -1,9 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../App";
 import Problem from "./Problem";
+import { getAllProblems } from "../../api/problemDataAPI";
+import formatStats from "../../utilities/formatStats";
+import { FilterContext } from "./index";
 
-const ProblemList = ({type}) => {
+const ProblemList = ({ type }) => {
   const { isLoggedIn } = useContext(AuthContext);
+  const { filterObj } = useContext(FilterContext);
+  const [problemList, setProblemList] = useState([]);
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await getAllProblems(filterObj);
+      const problems = data.problems;
+      setProblemList(() =>
+        problems.map((problem, i) => (
+          <Problem
+            key={i}
+            number={problem.number}
+            type={type}
+            name={problem.title}
+            acceptance={problem?.stats?.acceptance || 0}
+            difficulty={problem.difficulty}
+            userSubmissions="1"
+            submissions={formatStats(problem?.stats?.submissions) || 0}
+            status="solved"
+          />
+        ))
+      );
+    };
+    loadData();
+  }, [filterObj]);
   return (
     <div className="flex flex-col bg-secondary rounded-xl mb-3 grow overflow-clip">
       <div className="flex flex-row items-center p-3 text-md border-b-[1px] border-hover">
@@ -12,16 +39,12 @@ const ProblemList = ({type}) => {
         <div className="w-40">Acceptance</div>
         <div className="w-40">Difficulty</div>
         <div className="w-40">Submissions</div>
-        {isLoggedIn && type !== "select" && <div className="w-40">Your Submissions</div>}
+        {isLoggedIn && type !== "select" && (
+          <div className="w-40">Your Submissions</div>
+        )}
         {type === "select" && <div className="w-20">Selected</div>}
       </div>
-      <Problem number="1" type={type} name="Two Sum" acceptance="49.4" difficulty="easy" userSubmissions="1" submissions="18M" status="solved" />
-      <Problem number="2" type={type} name="Add Two Numbers" acceptance="40.1" difficulty="medium" userSubmissions="2" submissions="2M" status="attempted" />
-      <Problem number="3" type={type} name="Median of Two Sorted Arrays" acceptance="35.8" difficulty="hard" userSubmissions="7" submissions="232K" status="solved" />
-      <Problem number="4" type={type} name="Check Palindrome" acceptance="35.8" difficulty="easy" userSubmissions="0" submissions="76.3K" status="" />
-      <Problem number="5" type={type} name="ZigZag Conversion" acceptance="43.5" difficulty="medium" userSubmissions="3" submissions="12M" status="solved" />
-      <Problem number="6" type={type} name="Integer to Roman" acceptance="61.8" difficulty="medium" userSubmissions="1" submissions="1M" status="solved" />
-      <Problem number="7" type={type} name="3Sum" acceptance="32.4" difficulty="medium" userSubmissions="4" submissions="929K" status="attempted" />
+      {problemList}
     </div>
   );
 };
