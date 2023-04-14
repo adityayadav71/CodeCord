4620deac0ffa633f18c0234aae55a9748733b0cb
adityayadav71 <adityayadav1088@gmail.com>
2023-04-14 13:30:28 +0530

    completed join, create, and update controller

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,36 +1,76 @@
 const User = require("../models/userModel");
+const Room = require("../models/roomModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.createRoom = catchAsync(async (req, res, next) => {
-  const userId = req.user.id;
-  const io = req.app.locals.io;
-  const user = await User.findById(userId);
+  const { userId, roomId } = req.body;
+  let user = await User.findById(userId);
+  if (user?.activeRooms?.length < 10) {
+    // Create a new room
+    const room = await Room.create({
+      roomId,
+      owner: userId,
+      participants: [user._id],
+    });
 
-  // Check if user already has a socket id
-  if (user?.roomId) {
-    const sockets = await io.fetchSockets();
-    // User already has a room, so join them to that room
+    // Update User
+    user = await User.findByIdAndUpdate(userId, {
+      $push: { activeRooms: roomId },
+    });
   } else {
-    console.log("new connection");
-    // User doesn't have a room, so create a new one for them
-    io.on("connection", (socket) => {
-      socket.on("disconnect", function () {
-        console.log("disconnect: ", socket.id);
-      });
-      // Save this socket connection to user model
-      // Handle Create-room event
-      socket.on("create-room", async () => {
-        socket.join(socket.id, () => {
-          console.log("The user has joined the recently created room.");
-        });
-        // Update user room ID in user model
-        await User.findOneAndUpdate({ _id: userId }, { roomId: socket.id });
-        socket.emit("room-created", socket.id);
-      });
+    return res.status(403).json({
+      status: "failure",
+      result:
+        "Cannot create more than 1 room for a user. Leave other rooms before creating a new one.",
     });
   }
 
   res.status(200).json({
     status: "success",
+    result: user?.activeRooms ?? false,
+  });
+});
+
+exports.joinRoom = catchAsync(async (req, res, next) => {
+  const { userId, roomId } = req.body;
+  const room = await Room.findOne({ roomId: roomId });
+
+  if (!room)
+    return next(
+      new AppError("The invite code may have expired. Please try again.", 400)
+    );
+
+  if (room.participants.length < room.settings.participantsLimit) {
+    const room = await Room.findOneAndUpdate(
+      { roomId: roomId },
+      { $push: { participants: userId } }
+    );
+
+    // await User.findByIdAndUpdate(userId, { $push: { activeRooms: roomId } });
+
+    return res.status(200).json({
+      status: "success",
+      room,
+    });
+  } else {
+    return next(new AppError("Room is full. Please try later!", 403));
+  }
+});
+
+exports.updateRoom = catchAsync(async (req, res, next) => {
+  const { roomId, settings } = req.body;
+
+  const room = await Room.findOneAndUpdate(
+    { roomId: roomId },
+    { settings: settings }
+  );
+
+  if (!room)
+    return next(new AppError("Something went wrong. Please try again", 500));
+
+  return res.status(200).json({
+    status: "success",
+    room,
   });
 });
