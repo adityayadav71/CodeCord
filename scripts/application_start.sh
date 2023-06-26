3bdd28315d0e6c570668a8c90940b43829f22340
adityayadav71 <adityayadav1088@gmail.com>
2023-06-26 11:17:35 +0530

    updated application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,5 +9,5 @@ pm2 start npm --name "server" -- run "prod"
 # Change to the client dist directory
 cd  ../client/dist
 
-# Start the client using npm serve with pm2
-pm2 start http-server --name "client" -- -p 5173
+# Start the client using http-server with pm2
+pm2 start http-server --name "client" -- -p 5173 -a 0.0.0.0 -c-1 --proxy http://localhost:5173?/index.html
