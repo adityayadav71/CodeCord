fd33f78a3e7447425347ea24b64f365a05881e10
adityayadav71 <adityayadav1088@gmail.com>
2023-12-17 19:15:39 +0530

    Fixed pagination with Swiper, added count() method to apiFeatures

--- client/src/components/Problems/Pagination.jsx
+++ client/src/components/Problems/Pagination.jsx
@@ -2,14 +2,16 @@ import { useState, useEffect, useContext } from "react";
 import { FaChevronLeft, FaChevronRight, FaAngleDown } from "react-icons/fa";
 import { FilterContext } from "./index";
 import { RoomFilterContext } from "../Rooms/CreateRoom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
 
-const pagination = ({ filterInsideModal }) => {
+const Pagination = ({ totalPages, filterInsideModal }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPageLimit, setPerPageLimit] = useState(20);
   const [isLimitChangerActive, setLimitChangerActive] = useState();
-  const { filterObj, setFilterObj } = useContext(
-    filterInsideModal ? RoomFilterContext : FilterContext
-  );
+  const { setFilterObj } = useContext(filterInsideModal ? RoomFilterContext : FilterContext);
 
   const setLimit = (e) => {
     setLimitChangerActive(false);
@@ -28,31 +30,31 @@ const pagination = ({ filterInsideModal }) => {
 
   const pageButtons = [];
   const setPageButtons = () => {
-    for (let i = 1; i <= filterObj.totalPages; i++) {
+    for (let i = 1; i <= totalPages; i++) {
       pageButtons.push(
-        <button
-          key={i}
-          className={`flex flex-row items-center justify-center ${
-            currentPage === i ? "bg-accent1" : "bg-accent2"
-          } p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent1`}
-          onClick={() => setCurrentPage(i)}
-        >
-          {i}
-        </button>
+        <SwiperSlide key={i} onClick={() => setCurrentPage(i)}>
+          <button className={`flex flex-row items-center justify-center ${currentPage === i ? "bg-accent1" : "bg-accent2"} p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent1`}>
+            {i}
+          </button>
+        </SwiperSlide>
       );
     }
   };
+
   setPageButtons();
+
   const nextPage = () => {
-    if (currentPage + 1 <= filterObj.totalPages) {
+    if (currentPage + 1 <= totalPages) {
       setCurrentPage((prev) => prev + 1);
     }
   };
+
   const prevPage = () => {
     if (currentPage - 1 >= 1) {
       setCurrentPage((prev) => prev - 1);
     }
   };
+
   useEffect(() => {
     const closeDropdown = (event) => {
       if (!event.target.closest(".limit")) {
@@ -64,54 +66,65 @@ const pagination = ({ filterInsideModal }) => {
       document.removeEventListener("click", closeDropdown);
     };
   }, []);
+
   return (
-    <div className="flex flex-row p-3 gap-x-3 pl-20 bg-secondary w-full rounded-xl select-none">
-      <div
-        className="flex flex-row items-center justify-center bg-accent2 p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent1 mr-6"
+    <div className="flex flex-row p-2 gap-x-3 items-center bg-secondary w-full rounded-xl select-none">
+      <button
+        className="disabled:text-grey1 bg-transparent pagination-swiper-button-prev flex flex-row items-center justify-center p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent2"
         onClick={prevPage}
       >
         <FaChevronLeft />
-      </div>
-      {pageButtons}
-      <div
-        className="flex flex-row items-center justify-center bg-accent2 p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent1 ml-6"
+      </button>
+      <Swiper
+        className="relative m-0 w-[10rem] sm:w-[16rem] md:w-[20rem] lg:w-[36rem]"
+        navigation={{
+          nextEl: ".pagination-swiper-button-next",
+          prevEl: ".pagination-swiper-button-prev",
+        }}
+        modules={[Navigation]}
+        spaceBetween={1}
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+          },
+          374: {
+            slidesPerView: 3,
+          },
+          640: {
+            slidesPerView: 5,
+          },
+          1024: {
+            slidesPerView: 10,
+          },
+        }}
+      >
+        {pageButtons}
+      </Swiper>
+      <button
+        className="disabled:text-grey1 bg-transparent pagination-swiper-button-next flex flex-row items-center justify-center p-3 text-white rounded-xl w-10 h-10 hover:cursor-pointer hover:bg-accent2"
         onClick={nextPage}
       >
         <FaChevronRight />
-      </div>
+      </button>
       <div className="limit relative ml-auto">
         <div
-          className="flex flex-row items-center justify-center h-full bg-accent2 gap-x-3 px-3 text-white rounded-xl hover:cursor-pointer hover:bg-accent1"
+          className="flex flex-row items-center justify-center h-full w-36 bg-accent2 gap-x-3 py-1 px-3 text-white rounded-xl hover:cursor-pointer hover:bg-accent1"
           onClick={() => setLimitChangerActive((prev) => !prev)}
         >
           {perPageLimit} / Page <FaAngleDown />
         </div>
         <div
           className={`absolute transition-all duration-100 ${
-            isLimitChangerActive
-              ? "z-20  opacity-1 scale-100"
-              : "opacity-0 scale-0"
+            isLimitChangerActive ? "z-20  opacity-1 scale-100" : "opacity-0 scale-0"
           } bottom-12 shadow shadow-dropDown right-0 p-3 w-40 hover:cursor-pointer bg-secondary rounded-xl`}
         >
-          <div
-            className="flex flex-row items-center justify-between hover:bg-accent3 mb-3 rounded-lg px-3 py-1"
-            data-value="20"
-            onClick={setLimit}
-          >
+          <div className="flex flex-row items-center justify-between hover:bg-accent3 mb-3 rounded-lg px-3 py-1" data-value="20" onClick={setLimit}>
             20 / Page
           </div>
-          <div
-            className="flex flex-row items-center justify-between hover:bg-accent3 mb-3 rounded-lg px-3 py-1"
-            data-value="50"
-            onClick={setLimit}
-          >
+          <div className="flex flex-row items-center justify-between hover:bg-accent3 mb-3 rounded-lg px-3 py-1" data-value="50" onClick={setLimit}>
             50 / Page
           </div>
-          <div
-            className="flex flex-row items-center justify-between hover:bg-accent3 rounded-lg px-3 py-1"
-            data-value="100"
-            onClick={setLimit}
-          >
+          <div className="flex flex-row items-center justify-between hover:bg-accent3 rounded-lg px-3 py-1" data-value="100" onClick={setLimit}>
             100 / Page
           </div>
         </div>
@@ -120,4 +133,4 @@ const pagination = ({ filterInsideModal }) => {
   );
 };
 
-export default pagination;
+export default Pagination;
