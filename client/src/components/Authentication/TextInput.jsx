34883704a75dafc9209bb4e31b7888e84d687d17
adityayadav71 <adityayadav1088@gmail.com>
2024-02-25 20:54:02 +0530

    update logo

---
+++ client/src/components/Authentication/TextInput.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TextInput = ({placeholder}) => {
+  return (
+    <input className="w-full rounded-xl h-18 px-6 py-6 text-base focus:outline focus:outline-accent1 bg-grey3" placeholder={`${placeholder}`}></input>
+  )
+};
+
+export default TextInput;
