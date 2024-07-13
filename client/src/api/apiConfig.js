ace5d0d6932088fc077e2ece6ebfc3f6795e3fa0
Aditya Yadav <adityayadav1088@gmail.com>
2024-07-13 15:17:57 +0530

    Move deployment from AWS to Vercel/Render

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -1,9 +1,9 @@
 export const BASE_URL =
   import.meta.env.MODE === "production"
-    ? "https://codecord.tech/api/v1"
+    ? import.meta.env.VITE_API_URL
     : "/api/v1";
 
 export const SOCKET_URL = 
     import.meta.env.MODE === "production"
-      ? "https://codecord.tech/"
+      ? import.meta.env.VITE_SOCKET_URL
       : "http://localhost:5000/";
