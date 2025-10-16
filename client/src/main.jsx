45b40ae669a20855992c826498792347575d8b27
adityayadav71 <aditya.yadav@dhdigital.co.in>
2025-10-16 17:12:23 +0530

    chore: add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/tailwind.css";
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   /* </StrictMode> */
 );
